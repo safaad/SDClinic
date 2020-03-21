a204b6706ca8d48dfbccd8c08469950951e6e952
Safaa Dyab <33197552+safaad@users.noreply.github.com>
2020-03-22 01:10:43 +0200

    Add files via upload

---
+++ Models/Reminder_Insurance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace SDClinic.Models
+
+{
+    public class Reminder_Insurance
+    {
+        public int Id { get; set; }
+  
+        public DateTime? DateTime { get; set; }
+        [StringLength(300)]
+        public string Content { get; set; }
+        [StringLength(12)]
+        public string Priority { get; set; }
+        [StringLength(100)]
+        public string Title { get; set; }
+        //foreign key
+        public InsuranceCompany Insurance_Company { get; set; }
+        public String Insurance_CompanyName { get; set; }
+    }
+}
---
+++ Models/Report.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SDClinic.Models
+
+{
+    public class Report
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        //foreign key
+        [Required]
+        public int report_cons_id { get; set; }
+
+        [Required]
+        [ForeignKey("report_cons_id")]
+        public Consultation consultation;
+
+        [Required]
+        public string ReportId { get; set; }
+
+        [Required]
+        [ForeignKey("ReportId")]
+        public InsuranceCompany InsuranceCompany;
+
+
+    }
+}
